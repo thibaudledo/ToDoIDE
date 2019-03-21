d13d77d05389d6b2f242e745734d30b4bb94c8f7
Thibaud <thibaudledo@gmail.com>
2019-03-21 15:14:02 +0100

    Modifuing MainWindow
    Starting to fill MainWindow with items

--- src/view/src/main_window.cpp
+++ src/view/src/main_window.cpp
@@ -4,5 +4,19 @@ MainWindow::MainWindow(std::string &windowName)
     : m_windowName("ToDoIDE")
 {
     m_windowName = windowName;
+
+    m_menuBar = new QMenuBar(this);
+
+    setMenuBar(m_menuBar);
+}
+
+std::string MainWindow::getWindowName() const
+{
+    return m_windowName;
+}
+
+void MainWindow::setWindowName(const std::string &windowName)
+{
+    m_windowName = windowName;
 }
 
