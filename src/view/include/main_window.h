bc3084f8b8ebb7a88145e76f914daf86daea080d
Thibaud <thibaudledo@gmail.com>
2019-03-21 15:14:02 +0100

    Modifying MainWindow
    Starting to fill MainWindow with items

--- src/view/include/main_window.h
+++ src/view/include/main_window.h
@@ -4,15 +4,34 @@
 
 #include <string>
 
-
+#include <QPlainTextEdit>
+#include <QMenuBar>
+#include <QMenu>
+#include <QTreeView>
+#include <QAction>
 
 class MainWindow : public QMainWindow {
-    public:
-        MainWindow() = default;
-        MainWindow(std::string &windowName);
+public:
+    MainWindow() = default;
+    MainWindow(std::string &windowName);
 
-    private:
-        std::string m_windowName;
+    std::string getWindowName() const;
+    void setWindowName(const std::string &windowName);
+
+private:
+    std::string m_windowName;
+
+    QMenuBar* m_menuBar;
+
+    QMenu* m_fileMenu;
+    QMenu* editMenu;
+
+
+
+
+    QTreeView* m_treeView;
+    QPlainTextEdit* m_textEditor;
+
 
 
 };
